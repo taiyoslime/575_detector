ba5158dd233b266e34ff31f517591bc520ebb09d
taiyoslime <hase.tekitou@gmail.com>
2017-01-20 19:44:44 +0900

    字余りと 字足らずにね 対応 (字足らず)

--- main.rb
+++ main.rb
@@ -9,11 +9,12 @@ def detect_575 str, phrase = [5,7,5]
 	}
 	result = []
 	cp = -> (n,ctn,pos,res){
-		if ctn == phrase[n]
-			next res if n+1 == phrase.size
-			next cp.call(n+1,0,pos,res + " ")
-		elsif pos == ar.size || (ctn == 0 && (ar[pos][1] == ("助詞"||"助動詞") || (ar[pos][1] != ("動詞") && ar[pos][2] == ("非自立"||"接尾")))) || ctn > phrase[n]
+		p n,ctn,pos,res
+		if pos == ar.size || (ctn == 0 && (ar[pos][1] == ("助詞"||"助動詞") || (ar[pos][1] != ("動詞") && ar[pos][2] == ("非自立"||"接尾")))) || ctn > phrase[n]+1
 			next nil
+		elsif [*phrase[n]-1..phrase[n]+1].include?(ctn)
+			next res if n+1 == phrase.size
+			next cp.call(n+1,0,pos,res + " ") || cp.call(n,ctn+ar[pos][3],pos+1,res+ar[pos][0])
 		end
 		next cp.call(n,ctn+ar[pos][3],pos+1,res+ar[pos][0])
 	}
