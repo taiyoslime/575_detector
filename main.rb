93c8c06927d7355601a6de3df4659d1a64144db3
taiyoslime <hase.tekitou@gmail.com>
2017-01-20 19:21:03 +0900

    フレーズの 先頭条件 厳密化

--- main.rb
+++ main.rb
@@ -1,21 +1,21 @@
 def syllable_count str
-	str.gsub(/ャ|ュ|ョ/,"").size
+	str.gsub(/[ャュョ]/,"").size
 end
 
 def detect_575 str, phrase = [5,7,5]
 	ar = `echo #{str} | mecab`.split(/EOS|\n/).inject([]){ |res, e|
 		ln = e.split(/\t|,/);
-		res<<[ln[0],ln[1],(ln[9].nil? ? ln[0].size : syllable_count(ln[9]))]
+		res<<[ln[0],ln[1],ln[2],(ln[9].nil? ? ln[0].size : syllable_count(ln[9]))]
 	}
 	result = []
 	cp = -> (n,ctn,pos,res){
 		if ctn == phrase[n]
 			next res if n+1 == phrase.size
 			next cp.call(n+1,0,pos,res + " ")
-		elsif pos == ar.size || (ctn == 0 && ar[pos][1] == ("助詞"||"助動詞")) || ctn > phrase[n]
+		elsif pos == ar.size || (ctn == 0 && (ar[pos][1] == ("助詞"||"助動詞") || (ar[pos][1] != ("動詞") && ar[pos][2] == ("非自立"||"接尾")))) || ctn > phrase[n]
 			next nil
 		end
-		next cp.call(n,ctn+ar[pos][2],pos+1,res+ar[pos][0])
+		next cp.call(n,ctn+ar[pos][3],pos+1,res+ar[pos][0])
 	}
 	ar.size.times{ |e| result << cp.call(0,0,e,"") }
 
